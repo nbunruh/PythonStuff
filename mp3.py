562d57f3d641126ee8c63d0738eaa2f7fa2733d1
nbunruh <nolanbunruh@gmail.com>
2020-10-14 16:46:23 -0700

    added more functionality and interactivity

--- mp3.py
+++ mp3.py
@@ -1,14 +1,17 @@
-# Simple MP3 player written in Python
+# Simple CLI MP3 player written in Python
 
 import random
+from time import sleep
 
 class Mp3Player:
-    def __init__(self, model):
+    def __init__(self, owner, model):
+        self.owner = owner
         self.model = model
         self.library = {}
-
-    def greet(self, name):
-        print(f'Hello, {name}! Welcome to your {self.model}.')
+    
+    def greet(self):
+        print(f'Hello, {self.owner}! Welcome to your {self.model}')
+        print("What would you like to do?")
 
     def add(self, artist, title):
         if not artist in self.library:
@@ -24,3 +27,12 @@ class Mp3Player:
         song = random.choice(shuffle)
         print(f'Now playing: {song}')
 
+print("Turning on your device...")
+sleep(2)
+print("What is your name?")
+name = input()
+print("Which device are you using?")
+model = input()
+
+mp3 = Mp3Player(name, model)
+mp3.greet()
